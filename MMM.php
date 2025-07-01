3664510c1a20f0689d043b9e01f37442fbc58ff1
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-07-01 16:32:14 +0200

    Update MMM.php

--- MMM.php
+++ MMM.php
@@ -3,7 +3,7 @@
 # Type="MYSQL"
 # HTTP="true"
 $hostname_MMM = "localhost";
-$database_MMM = "matrixfundonline_mmm";
+$database_MMM = "matrixfudonline_mmm";
 $username_MMM = "matrixfundonline_root";
 $password_MMM = "root@wdp2017";
 $MMM = mysql_pconnect($hostname_MMM, $username_MMM, $password_MMM) or trigger_error(mysql_error(),E_USER_ERROR); 
